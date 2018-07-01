fc63648fb1bd27f54f4e956f925ec3bad0dd19b5
Ken Wong <canonw@users.noreply.github.com>
2018-07-01 22:46:45 +0000

    Add WIP roles

--- compile-default-vars.sh
+++ compile-default-vars.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+[[ -s "group_vars/all.yml" ]] && mv -f "group_vars/all.yml" "group_vars/all.yml~"
+
 cat \
   <(echo -e "---\n") \
+  <(cat group_vars/groups.yml | grep -v '^---$' | grep -v '^[[:space:]]*$') \
+  > group_vars/all.yml
+
+cat \
+  <(echo -e "\n# cat from roles defaults") \
   <(cat roles/*/defaults/main.yml | grep -v '^---$' | grep -v '^[[:space:]]*$') \
-  > group_vars/_compiled_defaults.yml
+  >> group_vars/all.yml
+
